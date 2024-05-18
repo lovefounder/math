08ebac290b5f83f42e82d32688eba7e95aacdd82
chengmei <2929773952@qq.com>
2024-05-18 15:29:46 +0800

    LP

---
+++ LP.py
@@ -0,0 +1,30 @@
+import pulp as pl
+import numpy as np
+
+for k in np.arange(0,0.271,0.001):
+    model = pl.LpProblem(name = 'My_model' , sense = pl.LpMinimize)
+
+    # define the decision variables
+    R = pl.LpVariable(name = "R")
+    x = {i:pl.LpVariable(name = f"x{i}",lowBound = 0 ,) for i in range(5)}
+
+    # add constraints
+    model += (R-0.025*x[1] >= 0)
+    model += (R-0.015*x[2] >= 0)
+    model += (R-0.055*x[3] >= 0)
+    model += (R-0.026*x[1] >= 0)
+    model += (x[0]+1.01*x[1]+1.02*x[2]+1.045*x[3]+1.065*x[4] == 1)
+    model += (0.05*x[0]+0.27*x[1]+0.19*x[2]+0.185*x[3]+0.185*x[4] >= k)
+
+    # Set the Objective
+    model += R
+
+    status = model.solve()
+
+    # Get the results
+    with open("optput.txt",'a') as f:
+        f.write(f"status: {model.status}, {pl.LpStatus[model.status]}\n")
+        f.write(f"objective: {model.objective.value()}\n")
+        
+        for var in model.variables():
+            f.write(f"{var.name}: {var.value()}\n")
